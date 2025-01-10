a4e7123a76c17bedbdf5f26f290750de3f80190e
Mohibalikhan <khanmohibali1@gmail.com>
2025-01-10 17:21:01 +0500

    updated

---
+++ src/sanity/schemaTypes/product.ts
@@ -0,0 +1,25 @@
+export default {
+    name: 'product',
+    type: 'document',
+    title: 'Product',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Product Name',
+      },
+      {
+        name: 'price',
+        type: 'number',
+        title: 'Product Price',
+      },
+      {
+        name: 'image',
+        type: 'image',
+        title: 'Product Image',
+        options: {
+          hotspot: true // Enables cropping and focal point selection
+        }
+      }
+    ]
+  };
