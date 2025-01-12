d52d690b08e3f1cb55eb109e16e5a408206bac2d
Mohibalikhan <khanmohibali1@gmail.com>
2025-01-13 00:54:15 +0500

    inshallah

--- src/sanity/schemaTypes/product.ts
+++ src/sanity/schemaTypes/product.ts
@@ -1,25 +1,27 @@
-export default {
-    name: 'product',
-    type: 'document',
-    title: 'Product',
-    fields: [
-      {
-        name: 'name',
-        type: 'string',
-        title: 'Product Name',
+const productSchema = {
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Product Name',
+    },
+    {
+      name: 'price',
+      type: 'number',
+      title: 'Product Price',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Product Image',
+      options: {
+        hotspot: true, // Enables cropping and focal point selection
       },
-      {
-        name: 'price',
-        type: 'number',
-        title: 'Product Price',
-      },
-      {
-        name: 'image',
-        type: 'image',
-        title: 'Product Image',
-        options: {
-          hotspot: true // Enables cropping and focal point selection
-        },
-      }
-    ]
-  };
+    },
+  ],
+};
+
+export default productSchema;
